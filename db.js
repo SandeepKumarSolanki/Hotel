a393e10da35ea8fb4e2c8cc2f856417d2418f45d
SandeepKumar <sandeepkusolanki@gamil.com>
2024-08-31 15:07:10 +0530

    local mongodb to online mongodb Atlas

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/hotels' //replace "hotels" with your database name
+//const mongoURL = process.env.MONGODB_URL_LOCAL //replace "hotels" with your database name
+const mongoURL = process.env.MONGODB_URL;
 
 //Set up MongoDB connection
 mongoose.connect(mongoURL , {
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express();
 const db = require('./db')
+require('dotenv').config();
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
 
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Welcome to our Hotel');
@@ -17,6 +20,8 @@ const menuItemRoutes = require('./routes/menuItemRoutes')
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000 , () => {
+
+
+app.listen(PORT , () => {
     console.log('Listening on port 3000')
 })
